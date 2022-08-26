896988a581d0583142fd1843798dac687efddb3f
Remco Westerhoud <remcowesterhoud@msn.com>
2022-08-26 20:44:33 +0200

    almost clean slate

--- src/main/java/com/westerhoud/osrs/taskman/TaskmanPlugin.java
+++ src/main/java/com/westerhoud/osrs/taskman/TaskmanPlugin.java
@@ -1,19 +1,10 @@
 package com.westerhoud.osrs.taskman;
 
 import com.google.inject.Provides;
-import com.westerhoud.osrs.taskman.domain.Account;
-import com.westerhoud.osrs.taskman.domain.AccountTask;
-import com.westerhoud.osrs.taskman.service.AuthenticationService;
-import com.westerhoud.osrs.taskman.service.TaskService;
 import com.westerhoud.osrs.taskman.ui.TaskmanPluginPanel;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.ClientToolbar;
@@ -22,9 +13,6 @@ import net.runelite.client.util.ImageUtil;
 
 import javax.inject.Inject;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URI;
-import java.util.Optional;
 
 @Slf4j
 @PluginDescriptor(name = "Taskman")
@@ -34,25 +22,11 @@ public class TaskmanPlugin extends Plugin {
   @Inject private TaskmanConfig config;
 
   private TaskmanPluginPanel sidePanel;
-  private AuthenticationService authenticationService;
-  private TaskService taskService;
-  private URI baseUri;
-  private Optional<Account> loggedInUser;
-
   @Override
-  protected void startUp() throws Exception {
-    log.info("Taskman started!");
-    baseUri = new URI(config.url());
-
-    authenticationService = new AuthenticationService(baseUri);
-    taskService = new TaskService(baseUri);
-
-    loggedInUser = authenticationService.login(config.username(), config.password());
-
+  protected void startUp() {
     // Sidebar
     final BufferedImage icon = ImageUtil.loadImageResource(getClass(), "icon.png");
-    sidePanel = new TaskmanPluginPanel(loggedInUser, taskService);
-
+    sidePanel = new TaskmanPluginPanel();
     clientToolbar.addNavigation(
         NavigationButton.builder()
             .tooltip("Taskman")
@@ -60,20 +34,6 @@ public class TaskmanPlugin extends Plugin {
             .icon(icon)
             .panel(sidePanel)
             .build());
-  }
-
-  @Override
-  protected void shutDown() throws Exception {
-    log.info("Taskman stopped!");
-  }
-
-  @Subscribe
-  public void onConfigChanged(final ConfigChanged configChanged) {
-    System.out.println("Config was changed!");
-    if (configChanged.getKey().equals("username") || configChanged.getKey().equals("password")) {
-      loggedInUser = authenticationService.login(config.username(), config.password());
-    }
-    sidePanel.reset(loggedInUser);
   }
 
   @Provides
--- src/main/java/com/westerhoud/osrs/taskman/ui/TaskmanPluginPanel.java
+++ src/main/java/com/westerhoud/osrs/taskman/ui/TaskmanPluginPanel.java
@@ -1,43 +1,15 @@
 package com.westerhoud.osrs.taskman.ui;
 
-import com.westerhoud.osrs.taskman.domain.Account;
-import com.westerhoud.osrs.taskman.service.TaskService;
 import net.runelite.client.ui.PluginPanel;
 
 import java.awt.*;
-import java.util.Optional;
 
-@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class TaskmanPluginPanel extends PluginPanel {
 
-  private final TaskService taskService;
 
-  public TaskmanPluginPanel(final Optional<Account> account, TaskService taskService) {
+  public TaskmanPluginPanel() {
     super();
-    this.taskService = taskService;
     setLayout(new BorderLayout());
-    account.ifPresentOrElse(this::addLoggedInPanels, this::addErrorPanels);
   }
 
-  private void addLoggedInPanels(Account account) {
-    LoggedUserPanel loggedUserPanel = new LoggedUserPanel(account);
-    CurrentTaskPanel currentTaskPanel = new CurrentTaskPanel(account, taskService);
-    ButtonsPanel buttonsPanel = new ButtonsPanel(account, taskService, loggedUserPanel, currentTaskPanel);
-
-    add(loggedUserPanel, BorderLayout.NORTH);
-    add(currentTaskPanel, BorderLayout.CENTER);
-    add(buttonsPanel, BorderLayout.SOUTH);
-  }
-
-  private void addErrorPanels() {
-    add(new ErrorPanel("Could not login. Please check verify that you have entered the correct credentials." +
-            " Contact us if the problem persists."), BorderLayout.CENTER);
-  }
-
-  public void reset(final Optional<Account> account) {
-    removeAll();
-    account.ifPresentOrElse(this::addLoggedInPanels, this::addErrorPanels);
-    revalidate();
-    repaint();
-  }
 }
