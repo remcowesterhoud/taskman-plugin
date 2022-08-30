4afd0f464698adce5e8a99b1d502415b4123e343
Remco Westerhoud <remcowesterhoud@msn.com>
2022-08-30 09:48:57 +0200

    downgrade to language level 8 and use OkHttp and Gson

--- src/main/java/com/westerhoud/osrs/taskman/domain/Progress.java
+++ src/main/java/com/westerhoud/osrs/taskman/domain/Progress.java
@@ -1,6 +1,5 @@
 package com.westerhoud.osrs.taskman.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +9,6 @@ import java.util.Map;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Progress {
   private Map<String, TierProgress> progressByTier;
   private String currentTier;
--- src/main/java/com/westerhoud/osrs/taskman/service/SheetService.java
+++ src/main/java/com/westerhoud/osrs/taskman/service/SheetService.java
@@ -1,123 +1,110 @@
 package com.westerhoud.osrs.taskman.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.westerhoud.osrs.taskman.domain.ErrorResponse;
 import com.westerhoud.osrs.taskman.domain.Progress;
 import com.westerhoud.osrs.taskman.domain.SheetRequestBody;
 import com.westerhoud.osrs.taskman.domain.Task;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @Slf4j
 public class SheetService {
 
   private static final String BASE_URL = "https://osrs-taskman.herokuapp.com/sheet";
-  private final HttpClient client;
+  final OkHttpClient client;
+  final Gson gson;
   private final String currentUrl;
-  private final URI generateUrl;
-  private final URI completeUrl;
+  private final String generateUrl;
+  private final String completeUrl;
   private final String progressUrl;
 
-  public SheetService() throws URISyntaxException {
-    client = HttpClient.newHttpClient();
+  public SheetService() {
+    this.client = new OkHttpClient();
+    this.gson = new Gson();
     this.currentUrl = BASE_URL + "/current";
-    this.generateUrl = new URI(BASE_URL + "/generate");
-    this.completeUrl = new URI(BASE_URL + "/complete");
+    this.generateUrl = BASE_URL + "/generate";
+    this.completeUrl = BASE_URL + "/complete";
     this.progressUrl = BASE_URL + "/progress";
   }
 
-  public Task getCurrentTask(final String key, final String passphrase)
-      throws URISyntaxException, IOException, InterruptedException {
+  public Task getCurrentTask(final String key, final String passphrase) throws IOException {
     if (key == null || key.isEmpty() || passphrase == null || passphrase.isEmpty()) {
       throw new IllegalArgumentException(
           "Please set your key and passphrase in the plugin configurations");
     }
 
-    final HttpRequest request;
-    request =
-        HttpRequest.newBuilder()
-            .uri(new URI(String.format("%s?key=%s&passphrase=%s", currentUrl, key, passphrase)))
-            .GET()
+    final Request request =
+        new Request.Builder()
+            .url(String.format("%s?key=%s&passphrase=%s", currentUrl, key, passphrase))
+            .get()
+            .build();
+
+    return executeRequest(request);
+  }
+
+  public Task generateTask(final String key, final String passphrase) throws IOException {
+    final Request request =
+        new Request.Builder()
+            .url(generateUrl)
+            .header("Content-Type", "application/json")
+            .post(getRequestBody(key, passphrase))
             .build();
     return executeRequest(request);
   }
 
-  public Task generateTask(final String key, final String passphrase)
-      throws IOException, InterruptedException {
-    final HttpRequest request;
-    request =
-        HttpRequest.newBuilder()
-            .uri(generateUrl)
-            .setHeader("Content-Type", "application/json")
-            .POST(getRequestBody(key, passphrase))
+  public Task completeTask(final String key, final String passphrase) throws IOException {
+    final Request request =
+        new Request.Builder()
+            .url(completeUrl)
+            .header("Content-Type", "application/json")
+            .post(getRequestBody(key, passphrase))
             .build();
     return executeRequest(request);
   }
 
-  public Task completeTask(final String key, final String passphrase)
-      throws IOException, InterruptedException {
-    final HttpRequest request;
-    request =
-        HttpRequest.newBuilder()
-            .uri(completeUrl)
-            .setHeader("Content-Type", "application/json")
-            .POST(getRequestBody(key, passphrase))
-            .build();
-    return executeRequest(request);
-  }
-
-  public Progress progress(final String key, final String passphrase)
-      throws URISyntaxException, IOException, InterruptedException {
-    final HttpRequest request;
-    request =
-        HttpRequest.newBuilder()
-            .uri(new URI(String.format("%s?key=%s&passphrase=%s", progressUrl, key, passphrase)))
-            .GET()
+  public Progress progress(final String key, final String passphrase) throws IOException {
+    final Request request =
+        new Request.Builder()
+            .url(String.format("%s?key=%s&passphrase=%s", progressUrl, key, passphrase))
+            .get()
             .build();
 
-    final HttpResponse<String> response =
-        client.send(request, HttpResponse.BodyHandlers.ofString());
+    final Response response = client.newCall(request).execute();
 
-    if (response.statusCode() == 200) {
-      return new ObjectMapper().readValue(response.body(), Progress.class);
+    if (response.code() == 200) {
+      return gson.fromJson(response.body().string(), Progress.class);
     }
 
-    log.error(response.body());
+    log.error(response.body().string());
     ErrorResponse error = mapResponseToErrorResponse(response);
     throw new IllegalArgumentException(error.getMessage());
   }
 
-  private HttpRequest.BodyPublisher getRequestBody(final String key, final String passphrase)
-      throws IOException {
+  private RequestBody getRequestBody(final String key, final String passphrase) {
     final SheetRequestBody body = new SheetRequestBody(key, passphrase);
-    return HttpRequest.BodyPublishers.ofString(new ObjectMapper().writeValueAsString(body));
+    return RequestBody.create(MediaType.parse("application/json"), gson.toJson(body));
   }
 
-  private Task executeRequest(HttpRequest request) throws IOException, InterruptedException {
-    final HttpResponse<String> response =
-        client.send(request, HttpResponse.BodyHandlers.ofString());
+  private Task executeRequest(final Request request) throws IOException {
+    final Response response = client.newCall(request).execute();
 
-    if (response.statusCode() == 200) {
+    if (response.code() == 200) {
       return mapResponseToTask(response);
     }
 
-    log.error(response.body());
+    log.error(response.body().string());
     ErrorResponse error = mapResponseToErrorResponse(response);
     throw new IllegalArgumentException(error.getMessage());
   }
 
-  private Task mapResponseToTask(final HttpResponse<String> response) throws IOException {
-    return new ObjectMapper().readValue(response.body(), Task.class);
+  private Task mapResponseToTask(final Response response) throws IOException {
+    return gson.fromJson(response.body().string(), Task.class);
   }
 
-  private ErrorResponse mapResponseToErrorResponse(final HttpResponse<String> response)
-      throws IOException {
-    return new ObjectMapper().readValue(response.body(), ErrorResponse.class);
+  private ErrorResponse mapResponseToErrorResponse(final Response response) throws IOException {
+    return gson.fromJson(response.body().string(), ErrorResponse.class);
   }
 }
